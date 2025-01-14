9799feedd95ca84d6eb22f2e9de2ebf2ab157779
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-01-14 11:45:40 -0600

    Install React-Sparklines. Initialize store. Add Provider to RootLayout.

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Provider store={store}>
+        <body>{children}</body>
+      </Provider>
     </html>
   );
 }
