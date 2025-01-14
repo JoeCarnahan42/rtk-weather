db197a2dc8878cdb2dfda00ac994db4779d9789e
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-01-14 13:37:29 -0600

    add table outline

--- app/components/Table.js
+++ app/components/Table.js
@@ -1,28 +1,41 @@
-import store from "../store/configureStore";
-
 export default function Table({ data }) {
-  return (
-    <table className="table table-bordered table-hover">
-      <thead>
-        <tr>
-          <th scope="col">City</th>
-          <th scope="col">Tempurature</th>
-          <th scope="col">Pressure</th>
-          <th scope="col">Humidity</th>
-        </tr>
-      </thead>
-      {data.map((weather) => {
-        return (
-          <tbody key={weather.city}>
-            <tr>
-              <th scope="row">{weather}</th>
-              <td>{weather}</td>
-              <td>{weather}</td>
-              <td>{weather}</td>
-            </tr>
-          </tbody>
-        );
-      })}
-    </table>
-  );
+  if (!data) {
+    return (
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th scope="col">City</th>
+            <th scope="col">Tempurature</th>
+            <th scope="col">Pressure</th>
+            <th scope="col">Humidity</th>
+          </tr>
+        </thead>
+      </table>
+    );
+  } else {
+    return (
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th scope="col">City</th>
+            <th scope="col">Tempurature</th>
+            <th scope="col">Pressure</th>
+            <th scope="col">Humidity</th>
+          </tr>
+        </thead>
+        {data.map((weather) => {
+          return (
+            <tbody key={weather.cityName}>
+              <tr>
+                <th scope="row">{weather.cityName}</th>
+                <td>{weather.temp}</td>
+                <td>{weather.press}</td>
+                <td>{weather.hum}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    );
+  }
 }
