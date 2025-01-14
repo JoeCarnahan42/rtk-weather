6232df4db2a15dbc932fb68c27afc68b86862106
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-01-14 13:46:56 -0600

    Install axios

--- app/components/Table.js
+++ app/components/Table.js
@@ -1,41 +1,45 @@
 export default function Table({ data }) {
   if (!data) {
     return (
-      <table className="table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th scope="col">City</th>
-            <th scope="col">Tempurature</th>
-            <th scope="col">Pressure</th>
-            <th scope="col">Humidity</th>
-          </tr>
-        </thead>
-      </table>
+      <div className="container">
+        <table className="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">City</th>
+              <th scope="col">Tempurature</th>
+              <th scope="col">Pressure</th>
+              <th scope="col">Humidity</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   } else {
     return (
-      <table className="table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th scope="col">City</th>
-            <th scope="col">Tempurature</th>
-            <th scope="col">Pressure</th>
-            <th scope="col">Humidity</th>
-          </tr>
-        </thead>
-        {data.map((weather) => {
-          return (
-            <tbody key={weather.cityName}>
-              <tr>
-                <th scope="row">{weather.cityName}</th>
-                <td>{weather.temp}</td>
-                <td>{weather.press}</td>
-                <td>{weather.hum}</td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </table>
+      <div className="container">
+        <table className="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">City</th>
+              <th scope="col">Tempurature</th>
+              <th scope="col">Pressure</th>
+              <th scope="col">Humidity</th>
+            </tr>
+          </thead>
+          {data.map((weather) => {
+            return (
+              <tbody key={weather.cityName}>
+                <tr>
+                  <th scope="row">{weather.cityName}</th>
+                  <td>{weather.temp}</td>
+                  <td>{weather.press}</td>
+                  <td>{weather.hum}</td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
     );
   }
 }
